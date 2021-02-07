ca9776163825d8b5a4ed1139573760dcee9340b6
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-02-07 17:01:22 +0330

    code of video 2 added

---
+++ video 2/Main.java
@@ -0,0 +1,28 @@
+
+
+
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println(4 + 3.8); 
+
+        System.out.println(4 - 2.1);
+
+        System.out.println(7 / 3);
+
+        System.out.println(4 * 4);
+
+        System.out.println(-5 + 8);
+
+
+        System.out.println(7 % 3);
+        System.out.println(-7 % 3);
+        System.out.println(7 % -3);
+        System.out.println(-7 % -3);
+
+        System.out.println(2 * (4 + 20) / 4);
+    
+    }
+}
