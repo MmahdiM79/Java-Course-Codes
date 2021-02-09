fbc399bc922f228a5b6613b9750cd3558e5b71ff
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-02-09 09:41:42 +0330

    code of video 3 added

---
+++ video 3/Main.java
@@ -0,0 +1,19 @@
+
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        byte a;
+        short b;
+        int c;
+        long d;
+
+        float e;
+        double f;
+
+        boolean g;
+
+        char h;
+    }
+}
