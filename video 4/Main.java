49db33362dbb2467ef360fc31716b61ca1422de3
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-02-10 12:01:49 +0330

    file of video 4 added

---
+++ video 4/Main.java
@@ -0,0 +1,23 @@
+
+
+
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        int a = 5;
+        a += 5;
+        a += 5;
+        a /= 5;
+        a %= 5;
+        a *= 5;
+
+        float b = (float)(a/2.0);
+
+        float c = 4.3f;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
