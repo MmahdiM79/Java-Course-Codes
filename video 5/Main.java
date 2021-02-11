88e54852a26649d89c5f39d69c31a41fff331fbe
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-02-11 07:56:33 +0330

    code of video 5 added

---
+++ video 5/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+
+        // byte num = in.nextByte();
+        // System.out.println(num * 3);
+
+        // int num2 = in.nextInt();
+        // System.out.println(num2 * 4);
+
+        // float num3 = in.nextFloat();
+        // double num4 = in.nextDouble();
+        // long num5 = in.nextLong();
+        // short num6 = in.nextShort();
+        // byte num7 = in.nextByte();
+
+        // boolean flag = in.nextBoolean();
+        // System.out.println(flag);
+    }
+}
