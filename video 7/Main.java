b5dd3f542faad33d2f57364b6abc1be6f08f4131
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-02-15 10:32:57 +0330

    file of video 7 added

---
+++ video 7/Main.java
@@ -0,0 +1,78 @@
+
+
+
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        int num1 = 6;
+        float num2 = 5f;
+
+
+        // <
+        if (num1 < num2) 
+        {
+            System.out.println("num1 < num2");
+        } 
+        else 
+        {
+            System.out.println("num1 >= num2");
+        }
+        
+
+        // <=
+        if (num1 <= num2) 
+        {
+            System.out.println("num1 <= num2");
+        } 
+        else 
+        {
+            System.out.println("num1 > num2");
+        }
+
+
+        // >
+        if (num1 > num2) 
+        {
+            System.out.println("num1 > num2");
+        } 
+        else 
+        {
+            System.out.println("num1 <= num2");
+        }
+
+
+        // >=
+        if (num1 >= num2) 
+        {
+            System.out.println("num1 >= num2");
+        } 
+        else 
+        {
+            System.out.println("num1 < num2");
+        }
+
+
+        // ==
+        if (num1 == num2) 
+        {
+            System.out.println("num1 == num2");
+        } 
+        else 
+        {
+            System.out.println("num1 != num2");
+        }
+
+
+        // !=
+        if (num1 != num2) 
+        {
+            System.out.println("num1 != num2");
+        } 
+        else 
+        {
+            System.out.println("num1 == num2");
+        }
+    }
+}
