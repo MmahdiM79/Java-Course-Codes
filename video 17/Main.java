6e4333cea0b48a57c62f67297bd27c617652528b
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-07-14 10:29:25 +0430

    sorce code of video 17 added

---
+++ video 17/Main.java
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        String s1 = "mahdi";
+        String s2 = new String();
+        
+        char[] chars = {'m', 'o', 'h', 'a', 'm', 'm', 'a', 'd'};
+        String s3 = new String(chars);
+        String s4 = new String(chars, 5, 3);
+
+
+        
+
+        // System.out.println(s1);
+        // System.out.println(s2);
+        // System.out.println(s3);
+        // System.out.println(s4);
+
+        // System.out.printf("s1 = %s %d %c %f %b %n", s1, 1, 'f', 1.2, true);
+
+        // System.out.println("s1 = " + s1);
+
+        // System.out.println("1 + 3 = " + 1 + 3);
+
+        // System.out.println("1 + 3 = " + (1 + 3));
+
+        // System.out.println(1 + 2 + 4 + " String");
+
+        // System.out.println(1 + 2 + 3 + " String " + 1 + 4);
+    }
+}
+
+
+
