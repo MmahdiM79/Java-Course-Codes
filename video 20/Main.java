8c5e4a601b212bccfc705326ea586214c6425a71
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-07-17 10:02:04 +0430

    vide 20 source code added

---
+++ video 20/Main.java
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        // String s1 = "hello";
+
+        // switch (s1)
+        // {
+        //     case "Hello":
+        //         System.out.println("yes");
+        //     break;
+
+        //     case "world":
+        //         System.out.println("no");
+        //     break;
+
+        //     default:
+        //         System.out.println("none");
+        // }
+
+
+        // String[] strings = {"hello world", "java course", "java Strings", 
+        //                     "methods of Strings", "java course of hello world"};
+
+
+        // for (String s: strings)
+        // {
+        //     if (s.startsWith("va", 2))
+        //         System.out.println("### " + s);
+
+        //     if (s.startsWith("java"))
+        //         System.out.println("!!! " + s);
+
+        //     if (s.endsWith("world"))
+        //         System.out.println(s + " @@@");
+        // }
+
+
+        // String s2 = "Hello world";
+        // String s3 = "hello ahanel";
+
+        // System.out.println((int)'w');
+        // System.out.println((int)'a');
+        // System.out.println(s2.compareToIgnoreCase(s3));
+
+        // System.out.println((int)'H');
+        // System.out.println((int)'h');
+        // System.out.println(s2.compareToIgnoreCase(s3));
+
+
+        String s4 = "this is a test";
+
+        System.out.println(s4.indexOf("t"));
+        System.out.println(s4.indexOf('t', 5));
+        System.out.println(s4.lastIndexOf('t'));
+        System.out.println(s4.lastIndexOf("t", 11));
+    }
+}
