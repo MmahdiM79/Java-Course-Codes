10e65b778c5efe40296ff2560ff6a81ec570dfe9
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-07-19 08:34:46 +0430

    source code of video 21 added

---
+++ video 21/Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        // String s1 = "hello world channel";
+        // System.out.println(Arrays.toString(s1.split(" ")) + "   len:" + s1.split(" ").length);
+        // System.out.println(Arrays.toString(s1.split(" ", 2)) + "   len:" + s1.split(" ", 2).length);
+        
+
+        // String s2 = "hello world channel";
+        // System.out.println(s2.substring(6));
+        // System.out.println(s2.substring(6, 11));
+
+
+        String s3 = "hello world";
+
+        // s3 = s3 + " channel";
+        // System.out.println(s3);
+
+        // s3.concat(" channel");
+        // System.out.println(s3);
+
+        String s4 = s3.concat(" channel");
+        System.out.println(s3);
+        System.out.println(s4);
+    }
+}
