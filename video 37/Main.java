a8709eb0b155e64c6205970ca549682c6eaf2fab
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-08-11 09:56:50 +0430

    source codes of video 37 added

---
+++ video 37/Main.java
@@ -0,0 +1,21 @@
+
+
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Message msg = new Message("mahdi", "ahmad", null);
+        // System.out.println(msg);
+
+        Object obj = new Object();
+
+        // Main m = new Main();
+        // System.out.println(m);
+    }
+}
+
+
+
+
+
