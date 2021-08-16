ac2260744ad8e8410b2d54e7f35ce47699f3a639
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-08-16 09:59:53 +0430

    source codes of video 43 added

---
+++ video 43/Main.java
@@ -0,0 +1,15 @@
+
+
+
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Test t = new Test();
+        B b = new B();
+
+        System.out.println(t.getI());
+        System.out.println(b.getJ());
+    }
+}
---
+++ video 43/Test.java
@@ -0,0 +1,27 @@
+
+
+
+class B
+{
+    private int j = 0;
+
+    public int getJ() {
+        return j;
+    }
+
+    public void mines() { j--; }
+}
+
+
+
+public class Test 
+{
+    private int i = 0;
+
+
+    public int getI() {
+        return i;
+    }
+
+    public void plus() { i++; }
+}
