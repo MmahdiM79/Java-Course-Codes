4d9b81398e317215b8fb4b4e599ded21b3e49d66
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-08-25 10:28:07 +0430

    source code of video 50 added

---
+++ video 50/Main.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+
+
+
+
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(3); set.add(4); set.add(6); set.add(7); 
+
+
+        // set.add(3);
+        // System.out.println(set);
+
+
+        // System.out.println(set.contains(7));
+        // System.out.println(set.contains(9));
+
+
+        // set.remove(4);
+        // System.out.println(set);
+
+
+        // set.add(0);
+        // System.out.println(set);
+    }
+}
